3ea30c6d0197a6dccef99f3755f416e37f5edd11
Mikhail Belyaev <mbelyaev78@gmail.com>
2025-01-31 18:08:25 +0300

    DRAFT costimize test classes (2)

--- src/test/java/ru/hogwarts/school/service/FacultyServiceTest.java
+++ src/test/java/ru/hogwarts/school/service/FacultyServiceTest.java
@@ -33,13 +33,13 @@ public class FacultyServiceTest {
         verify(facultyRepositoryMock, times(1)).save(FACULTY_SOURCE_1);
     }
 
-    @Test
-    @Order(1)
-    public void should_findFaculty_succeed() {
-        when(facultyRepositoryMock.findById(FACULTY_ID_1)).thenReturn(Optional.of(FACULTY_TARGET_1));
-        assertEquals(FACULTY_TARGET_1, out.findFaculty(FACULTY_TARGET_1.getId()));
-        verify(facultyRepositoryMock, times(1)).findById(FACULTY_ID_1);
-    }
+//    @Test
+//    @Order(1)
+//    public void should_findFaculty_succeed() {
+//        when(facultyRepositoryMock.findById(FACULTY_ID_1)).thenReturn(Optional.of(FACULTY_TARGET_1));
+//        assertEquals(FACULTY_TARGET_1, out.findFaculty(FACULTY_TARGET_1.getId()));
+//        verify(facultyRepositoryMock, times(1)).findById(FACULTY_ID_1);
+//    }
 
     @Test
     @Order(1)
@@ -47,23 +47,15 @@ public class FacultyServiceTest {
         when(facultyRepositoryMock.findById(FACULTY_ID_1)).thenReturn(Optional.of(new Faculty()));
         assertEquals(new Faculty(), out.findFaculty(FACULTY_ID_1));
     }
-//
-//    @Test
-//    @Order(3)
-//    public void should_editFaculty_succeed() {
-//        out.addFaculty(FACULTY_1);
-//        final Faculty facultyActual = out.editFaculty(new Faculty(FACULTY_1.getId(),
-//                FACULTY_2.getName(),
-//                FACULTY_2.getColor()));
-//        assertEquals(out.findFaculty(FACULTY_1.getId()), facultyActual);
-//    }
-//
-//    @Test
-//    @Order(1)
-//    public void should_editFaculty_not_found() {
-//        assertNull(out.editFaculty(FACULTY_1));
-//    }
-//
+
+    @Test
+    @Order(1)
+    public void should_editFaculty_succeed() {
+        when(facultyRepositoryMock.save(eq(FACULTY_TARGET_1))).thenReturn(FACULTY_TARGET_1);
+        assertEquals(FACULTY_TARGET_1, out.addFaculty(FACULTY_TARGET_1));
+        verify(facultyRepositoryMock, times(1)).save(FACULTY_TARGET_1);
+    }
+
 //    @Test
 //    @Order(2)
 //    public void should_deleteFaculty_succeed() {
