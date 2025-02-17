7bcc680fb12bf9be55a02ca5b51832ab5a1a24a0
Mikhail Belyaev <mbelyaev78@gmail.com>
2025-02-17 08:23:08 +0300

    WebMvcTest complete

--- src/test/java/ru/hogwarts/school/controller/WebMvcTest/FacultyControllerTest.java
+++ src/test/java/ru/hogwarts/school/controller/WebMvcTest/FacultyControllerTest.java
@@ -1,4 +1,4 @@
-package ru.hogwarts.school.controller;
+package ru.hogwarts.school.controller.WebMvcTest;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
@@ -8,6 +8,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import ru.hogwarts.school.controller.FacultyController;
 import ru.hogwarts.school.exception.FacultyNotFoundException;
 import ru.hogwarts.school.model.Faculty;
 import ru.hogwarts.school.service.FacultyService;
