18193ab11d5d8433ab2ecca6a358b83f7014fa3b
Mikhail Belyaev <mbelyaev78@gmail.com>
2025-02-25 15:54:06 +0300

    refactoring

--- src/main/java/ru/hogwarts/school/service/StudentServiceImpl.java
+++ src/main/java/ru/hogwarts/school/service/StudentServiceImpl.java
@@ -1,33 +1,21 @@
 package ru.hogwarts.school.service;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.multipart.MultipartFile;
-import ru.hogwarts.school.model.Avatar;
 import ru.hogwarts.school.model.Student;
-import ru.hogwarts.school.repository.AvatarRepository;
 import ru.hogwarts.school.repository.StudentRepository;
 
-import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.Collection;
 import java.util.List;
-import java.util.Objects;
-
-import static java.nio.file.StandardOpenOption.CREATE_NEW;
 
 @Service
 @Transactional
 public class StudentServiceImpl implements StudentService {
 
     final StudentRepository studentRepository;
-    final AvatarRepository avatarRepository;
 
-    public StudentServiceImpl(StudentRepository studentRepository, AvatarRepository avatarRepository) {
+    public StudentServiceImpl(StudentRepository studentRepository) {
         this.studentRepository = studentRepository;
-        this.avatarRepository = avatarRepository;
     }
 
     public List<Student> findAll() {
