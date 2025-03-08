7edae63ebeb6ebad8dd6f9662ea2c512df56cf8a
Mikhail Belyaev <mbelyaev78@gmail.com>
2025-03-08 23:05:22 +0300

    refactoring

--- src/main/java/ru/hogwarts/school/controller/AvatarController.java
+++ src/main/java/ru/hogwarts/school/controller/AvatarController.java
@@ -16,7 +16,6 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
