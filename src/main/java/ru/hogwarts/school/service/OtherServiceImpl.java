b7939f27005413c3b5224f10cd51768cfe6ad9ae
Mikhail Belyaev <mbelyaev78@gmail.com>
2025-03-09 10:11:59 +0300

    refactoring

--- src/main/java/ru/hogwarts/school/service/OtherServiceImpl.java
+++ src/main/java/ru/hogwarts/school/service/OtherServiceImpl.java
@@ -8,9 +8,12 @@ import java.util.stream.IntStream;
 @Service
 public class OtherServiceImpl implements OtherService {
     public Optional<Integer> getIterableSequenceSum(Integer limitParam) {
-        return Optional.of(IntStream // Unlike Stream, IntStream is a stream of primitive int values
-                .iterate(1, a -> a + 1)
-                .limit(limitParam)
-                .reduce(0, Integer::sum));
+        // Unlike Stream, IntStream is a stream of primitive int values
+        // rangeClosed(int startInclusive, int endInclusive) returns a sequential ordered IntStream from
+        // startInclusive (inclusive) to endInclusive (inclusive) by an incremental step of 1
+        return Optional.of(IntStream
+                .rangeClosed(1, limitParam)
+                .parallel()
+                .sum());
     }
 }
